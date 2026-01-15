9f9995faf89d91c0a60b98db890490259746d36c
ambrogiocorti <s.ambrogiocorti@gmail.com>
2026-01-15 06:38:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,26 @@
+const CACHE_NAME = 'travel-hub-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Installazione: scarica i file
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('travel-hub-store').then((cache) => cache.addAll([
-      '/',
-      '/index.html',
-    ])),
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
   );
 });
 
+// Richiesta: serve i file dalla cache se offline
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
